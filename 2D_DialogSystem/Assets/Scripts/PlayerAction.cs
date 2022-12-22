4f8a9ed5ad513bcb9e230c2c469e67ae03050425
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2022-12-22 21:56:49 +0900

    Create Player

--- 2D_DialogSystem/Assets/Scripts/PlayerAction.cs
+++ 2D_DialogSystem/Assets/Scripts/PlayerAction.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerAction : MonoBehaviour
 {
+    [SerializeField]
+    private float speed;
+
+    float h;
+    float v;
+    Rigidbody2D rigid;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(h, v);
+    }
+
+    void Move()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
     }
 }
